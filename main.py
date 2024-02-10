5d4a6efcc5abd6d74e94bf847819d2861133c896
DMYomal <yomalsd123@gmail.com>
2024-02-10 17:07:42 +0000

    Use the slices and finalise the program

--- main.py
+++ main.py
@@ -39,10 +39,8 @@ while game_is_on:
         scoreboard.game_over()
 
     #Detect Collision with snake's tail
-    for segment in snake.segment_list:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment)<10:
+    for segment in snake.segment_list[1:]:
+        if snake.head.distance(segment)<10:
             game_is_on = False
             scoreboard.game_over()
 
